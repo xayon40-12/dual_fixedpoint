6c127b45a4eabf453c3a7282c0e8e43a35f3e47d
xayon40-12 <touroux.nathan@gmail.com>
2025-10-04 21:00:06 +0200

    working comparison of dual and dual of dual in Love2D

---
+++ main.lua
@@ -0,0 +1,227 @@
+Dual = {}
+
+function Dual:new(a, b)
+	return setmetatable({ a = a, b = b }, {
+		__index = Dual,
+		__add = function(l, r)
+			return Dual:new(l.a + r.a, l.b + r.b)
+		end,
+		__sub = function(l, r)
+			return Dual:new(l.a - r.a, l.b - r.b)
+		end,
+		__mul = function(l, r)
+			return Dual:new(l.a * r.a, l.a * r.b + r.a * l.b)
+		end,
+		__div = function(l, r)
+			local x = l.a / r.a
+			return Dual:new(x, (l.b - r.b * l.b * x) / r.a)
+		end,
+	})
+end
+
+function Dual:one()
+	if type(self.a) == "table" then
+		return Dual:new(self.a:one(), self.b:zero())
+	else
+		return Dual:new(1, 0)
+	end
+end
+
+function Dual:zero()
+	if type(self.a) == "table" then
+		return Dual:new(self.a:zero(), self.b:zero())
+	else
+		return Dual:new(0, 0)
+	end
+end
+
+Complex = {}
+
+function Complex:new(a, b)
+	return setmetatable({ a = a, b = b }, {
+		__index = Complex,
+		__add = function(l, r)
+			return Complex:new(l.a + r.a, l.b + r.b)
+		end,
+		__sub = function(l, r)
+			return Complex:new(l.a - r.a, l.b - r.b)
+		end,
+		__mul = function(l, r)
+			return Complex:new(l.a * r.a - l.b * r.b, l.a * r.b + r.a * l.b)
+		end,
+		__div = function(l, r)
+			local n = r:abs()
+			return Complex:new((l.a * r.a + l.b * r.b) / n, (l.b * r.a - l.a * r.b) / n)
+		end,
+	})
+end
+
+function Complex:one()
+	if type(self.a) == "table" then
+		return Complex:new(self.a:one(), self.b:zero())
+	else
+		return Complex:new(1, 0)
+	end
+end
+
+function Complex:zero()
+	if type(self.a) == "table" then
+		return Complex:new(self.a:zero(), self.b:zero())
+	else
+		return Complex:new(0, 0)
+	end
+end
+
+function Complex:abs()
+	return self.a * self.a + self.b * self.b
+end
+
+function Complex:conj()
+	return Complex:new(self.a, -self.b)
+end
+
+function Complex:recip()
+	local n = self:abs()
+	return Complex:new(self.a / n, -self.b / n)
+end
+
+function Complex:sqrt()
+	local n = math.sqrt(math.sqrt(self:abs()))
+	local t = math.atan2(self.b, self.a) * 0.5
+	return Complex:new(n * math.cos(t), n * math.sin(t))
+end
+
+function Complex:recip_zero()
+	return function(z0, z)
+		return z * z0 - z0:one()
+	end
+end
+
+function Complex:sqrt_zero()
+	return function(z0, z)
+		return z * z - z0
+	end
+end
+
+function hsl(h, s, l)
+	h = h / 360
+	s = s / 100
+	l = l / 100
+
+	local r, g, b
+
+	if s == 0 then
+		r, g, b = l, l, l -- achromatic
+	else
+		local function hue2rgb(p, q, t)
+			if t < 0 then
+				t = t + 1
+			end
+			if t > 1 then
+				t = t - 1
+			end
+			if t < 1 / 6 then
+				return p + (q - p) * 6 * t
+			end
+			if t < 1 / 2 then
+				return q
+			end
+			if t < 2 / 3 then
+				return p + (q - p) * (2 / 3 - t) * 6
+			end
+			return p
+		end
+
+		local q = l < 0.5 and l * (1 + s) or l + s - l * s
+		local p = 2 * l - q
+		r = hue2rgb(p, q, h + 1 / 3)
+		g = hue2rgb(p, q, h)
+		b = hue2rgb(p, q, h - 1 / 3)
+	end
+
+	local a = 1
+	return function()
+		return r, g, b, a
+	end
+end
+
+function love.load()
+	S = {}
+	S.cz = Complex:new(0, 0)
+	S.r = 10
+	S.s = 2
+	S.col = { hsl(0, 90, 70), hsl(100, 90, 70) }
+	S.col_1 = hsl(200, 90, 70)
+	S.col_2 = hsl(300, 90, 70)
+end
+
+function love.update(dt) end
+
+function love.draw()
+	local w = love.graphics.getWidth()
+	local h = love.graphics.getHeight()
+	local tx = function(x)
+		return (x / (2 * S.s) + 0.5) * w
+	end
+	local ty = function(y)
+		return (y / (2 * S.s * h / w) + 0.5) * h
+	end
+
+	love.graphics.setColor(1, 1, 1, 1)
+	love.graphics.circle("fill", tx(0), ty(0), S.r, 5)
+	for i, z in ipairs({ S.cz, S.cz:sqrt() }) do
+		love.graphics.setColor(S.col[i]())
+		love.graphics.circle("fill", tx(z.a), ty(z.b), S.r, 10)
+	end
+
+	local eps = 0.001
+	local f = Complex:sqrt_zero()
+	local z0 = S.cz
+	local start = Complex:new(1, 0)
+	local zp = start
+	local d = f(z0, zp)
+	local z
+	love.graphics.setColor(S.col_1())
+	local m1 = 0
+	for i = 1, 100 do
+		local du = f(Dual:new(z0, Complex:zero()), Dual:new(zp, d))
+		z = zp - d * (du.a * du.b:conj() / (du.b * du.b:conj()))
+		d = du.a
+		love.graphics.line(tx(zp.a), ty(zp.b), tx(z.a), ty(z.b))
+		zp = z
+		if d:abs() < eps then
+			m1 = i
+			break
+		end
+	end
+
+	zp = start
+	d = f(z0, zp)
+	love.graphics.setColor(S.col_2())
+	local m2 = 0
+	for i = 1, 100 do
+		local ddu = f(
+			Dual:new(Dual:new(z0, Complex:zero()), Dual:new(Complex:zero(), Complex:zero())),
+			Dual:new(Dual:new(zp, d), Dual:new(d, Complex:zero()))
+		)
+		d = d * (ddu.a.a * ddu.a.b:conj() / (ddu.a.b * ddu.a.b:conj() + ddu.a.a * ddu.b.b:conj()))
+		z = zp - d
+		d = ddu.a.a
+		love.graphics.line(tx(zp.a), ty(zp.b), tx(z.a), ty(z.b))
+		zp = z
+		if d:abs() < eps then
+			m2 = i
+			break
+		end
+	end
+
+	love.graphics.print("Order 1: " .. tostring(m1), 10, 10)
+	love.graphics.print("Order 2: " .. tostring(m2), 10, 20)
+end
+
+function love.mousemoved(x, y, dx, dy, istouch)
+	local w = love.graphics.getWidth()
+	local h = love.graphics.getHeight()
+	S.cz.a = (x / w - 0.5) * 2 * S.s
+	S.cz.b = (y / h - 0.5) * 2 * S.s * h / w
+end
